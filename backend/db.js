d051de8eb26a70f45ffa05af6e0d9390dc3cb89f
sherlocked6 <saichandu.sambangi@gmail.com>
2024-07-07 11:39:35 +0530

    removed mongo URL

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://saichandusambangi:h3VEciOpvN2VJ9a4@cluster0.pf9i2p0.mongodb.net/todos")
+mongoose.connect("Your mongo URL")
 
 const todoSchema = mongoose.Schema({
     title: String,
